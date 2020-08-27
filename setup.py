490e3d10283144063430785847c7dd1a0dbdd937
Jan-Oliver Joswig <jan.joswig@fu-berlin.de>
2020-08-27 17:51:33 +0200

    Bump version: 0.3.10 → 0.3.11

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ for category, fname in requirements_map.items():
 
 setup(
     name='cnnclustering',
-    version='0.3.10',
+    version='0.3.11',
     keywords=["Density-based-clustering"],
     scripts=["cnnclustering/cnn.py",
              "cnnclustering/cmsm.py",
