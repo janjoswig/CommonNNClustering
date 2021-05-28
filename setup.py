37105af8de641d5669c8fdd9cc69e46ee00409e3
Jan-Oliver Joswig <jan.joswig@fu-berlin.de>
2021-05-28 09:42:13 +0200

    Bump version: 0.4.0 â 0.4.1

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ for category, fname in requirements_map.items():
 
 setup(
     name="cnnclustering",
-    version="0.4.0",
+    version="0.4.1",
     keywords=["Density-based clustering"],
     author="Jan-Oliver Joswig",
     author_email="jan.joswig@fu-berlin.de",
